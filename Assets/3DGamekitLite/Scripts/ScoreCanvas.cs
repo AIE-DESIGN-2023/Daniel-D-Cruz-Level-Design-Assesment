a0e43ea3402fd28bab68af597c12275eafced634
AIE\s230169 <s230169@students.aie.edu.au>
2023-04-20 16:28:17 +0930

    Commit after day 2 in class. Section 4 completed, lighting begun.

---
+++ Assets/3DGamekitLite/Scripts/ScoreCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreCanvas : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    private float currentScore = 0;
+
+    public void UpdateScore(float score)
+    {
+        currentScore += score;
+        scoreText.text = currentScore.ToString();
+    }
+}
---
+++ Assets/3DGamekitLite/Scripts/ScoreObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreObject : MonoBehaviour
+{
+    public float scoreToAdd = 10;
+    private ScoreCanvas scoreCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreCanvas = FindObjectOfType<ScoreCanvas>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.GetComponent<CharacterController>())
+        //if(other.transform.tag == "Player" || other.gameObject.layer == 9)
+        {
+            scoreCanvas.UpdateScore(scoreToAdd);
+            Destroy(this.gameObject);
+        }
+    }
+}
